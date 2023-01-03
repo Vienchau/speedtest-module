33e5879afd559cd0978d8f4f7cde3ef669bb6380
vienchau <chautrivien2103@gmail.com>
2023-01-03 17:22:23 +0700

    init commit

---
+++ Makefile
@@ -0,0 +1,18 @@
+#CROSS_COMPILE =/home/vienchau/toolchain/HomeWifi/msdk-4.8.5-mips-EB-3.18-u0.9.33-m32ut-170828_lunapro-171213/msdk-4.8.5-mips-EB-3.18-u0.9.33-m32ut-170828_lunapro-171213/bin/mips-linux-uclibc-
+CC = gcc
+
+LIBRARY_PATH =/home/vienchau/build/local/lib
+INCLUDE_PATH = /home/vienchau/build/local/include
+
+CFLAGS =-I$(INCLUDE_PATH) -s -O3 -fPIC \
+	-pipe -fwrapv -fdata-sections  \
+	-ffunction-sections -finline-functions \
+	
+	
+LDFLAGS =-L$(LIBRARY_PATH) -lpthread -lcurl -lexpat -lm -lssl -lcrypto 
+
+speedtest_module: speedtest.c
+	$(CC) $< $(LDFLAGS)  $(CFLAGS) -o $@
+
+clean:
+	rm speedtest_module
---
+++ speedtest.c
@@ -0,0 +1,223 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <float.h>
+#include <math.h>
+#include <sys/stat.h>
+#include "curl/curl.h"
+
+#define TEST_COUNTER 10
+#define TEST_SIZE 1000000
+#define URL_CURL_DOWNLOAD_SSL "https://speedtest.fpt.vn.prod.hosts.ooklaserver.net:8080/download?size=10000000"
+#define URL_CURL_UPLOAD_SSL "https://speedtest.fpt.vn.prod.hosts.ooklaserver.net:8080/speedtest/upload"
+
+#define OUTFILE_CURL "test_download"
+#define FILE_NAME "file"
+
+struct speed_t
+{
+    curl_off_t speed_download;
+    curl_off_t total_time;
+
+    char filename[7];
+};
+
+size_t write_data(void *ptr, size_t size, size_t nmemb, FILE *stream)
+{
+    size_t written = fwrite(ptr, size, nmemb, stream);
+    return written;
+}
+
+static int calc_past_time(struct timeval *start, struct timeval *end)
+{
+    return (end->tv_sec - start->tv_sec) * 1000 + (end->tv_usec - start->tv_usec) / 1000;
+}
+
+static void *do_download(void *ptr)
+{
+    struct speed_t *speed = (struct speed_t *)ptr;
+    CURL *curl;
+    FILE *fp;
+    CURLcode res;
+    char outfilename[7];
+    strcpy(outfilename, speed->filename);
+    curl = curl_easy_init();
+    int time;
+    if (curl)
+    {
+
+        fp = fopen(outfilename, "wb");
+        curl_easy_setopt(curl, CURLOPT_URL, URL_CURL_DOWNLOAD_SSL);
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
+        curl_easy_setopt(curl, CURLOPT_USERAGENT, "speedtest");
+        curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
+        res = curl_easy_perform(curl);
+    }
+
+    if (res == CURLE_OK)
+    {
+        curl_easy_getinfo(curl, CURLINFO_SPEED_DOWNLOAD_T, &(speed->speed_download));
+        curl_easy_getinfo(curl, CURLINFO_TOTAL_TIME_T, &(speed->total_time));
+        fprintf(stderr, "Download speed: %0.2f Mb/s during %lu.%06lu seconds\n",
+                (double)(speed->speed_download / (double)1024 / (double)1024),
+                (unsigned long)(speed->total_time / 1000000),
+                (unsigned long)(speed->total_time % 1000000));
+    }
+    curl_easy_cleanup(curl);
+    fclose(fp);
+}
+
+double speedtest_download()
+{
+    struct timeval before_time, after_time;
+    // fclose(fp);
+    struct speed_t speed_arr[TEST_COUNTER];
+    pthread_t threads[TEST_COUNTER];
+    int rc;
+    pthread_attr_t attr;
+    void *status;
+
+    double sum;
+    double time;
+
+    for (int i = 0; i < TEST_COUNTER; i++)
+    {
+        strcpy(speed_arr[i].filename, FILE_NAME);
+    }
+    gettimeofday(&before_time, NULL);
+    for (int i = 0; i < TEST_COUNTER; i++)
+    {
+        rc = pthread_create(&threads[i], NULL, do_download, (void *)&speed_arr[i]);
+        if (rc)
+        {
+            printf("cant create thread!\n");
+        }
+    }
+    for (int i = 0; i < TEST_COUNTER; i++)
+    {
+        rc = pthread_join(threads[i], &status);
+        if (rc)
+        {
+            printf("cant join thread!\n");
+        }
+    }
+    gettimeofday(&after_time, NULL);
+    printf("start time: %ld\n", before_time.tv_sec * 1000 + before_time.tv_usec * 1000);
+    printf("start time: %ld\n", after_time.tv_sec * 1000 + after_time.tv_usec * 1000);
+    double time_hand = (double)(calc_past_time(&before_time, &after_time) / (double)1000);
+    for (int i = 0; i < TEST_COUNTER; i++)
+    {
+        sum += (double)(speed_arr[i].speed_download / (double)1024 / (double)1024);
+        time += (double)(speed_arr[i].total_time / (double)1000000);
+    }
+
+    time = time / (double)TEST_COUNTER;
+    printf("Total time: %f seconds\n", time_hand);
+    double final_speed = (double)(10 * (double)TEST_COUNTER / (double)time_hand) * 8;
+    return final_speed;
+}
+
+static void *do_upload(void *ptr)
+{
+    struct speed_t *speed = (struct speed_t *)ptr;
+    CURL *curl;
+    CURLcode res;
+    FILE *fd;
+    struct stat file_info;
+
+    fd = fopen(FILE_NAME, "r");
+    if (!fd)
+    {
+        printf("file open fail\n");
+        return NULL;
+    }
+
+    if (fstat(fileno(fd), &file_info) != 0)
+    {
+        printf("file open empty\n");
+        return NULL;
+    }
+    curl = curl_easy_init();
+
+    curl_easy_setopt(curl, CURLOPT_URL, URL_CURL_UPLOAD_SSL);
+    curl_easy_setopt(curl, CURLOPT_POST, 1L);
+    curl_easy_setopt(curl, CURLOPT_UPLOAD, 1L);
+    curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, "POST");
+    curl_easy_setopt(curl, CURLOPT_READDATA, fd);
+    curl_easy_setopt(curl, CURLOPT_INFILESIZE_LARGE, (curl_off_t)file_info.st_size);
+    curl_easy_setopt(curl, CURLOPT_USERAGENT, "speedtest upload");
+    res = curl_easy_perform(curl);
+    if (res != CURLE_OK)
+    {
+        fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
+    }
+    else
+    {
+        curl_easy_getinfo(curl, CURLINFO_SPEED_UPLOAD_T, &(speed->speed_download));
+        curl_easy_getinfo(curl, CURLINFO_TOTAL_TIME_T, &(speed->total_time));
+        fprintf(stderr, "Upload speed: %0.2f Mb/s during %lu.%06lu seconds\n",
+                (double)(speed->speed_download / (double)1024 / (double)1024),
+                (unsigned long)(speed->total_time / 1000000),
+                (unsigned long)(speed->total_time % 1000000));
+    }
+    curl_easy_cleanup(curl);
+}
+
+double speedtest_upload()
+{
+    struct timeval before_time, after_time;
+    struct speed_t speed_arr[TEST_COUNTER];
+    pthread_t threads[TEST_COUNTER];
+    int rc;
+    pthread_attr_t attr;
+    void *status;
+
+    double sum;
+    double time;
+    for (int i = 0; i < TEST_COUNTER; i++)
+    {
+        strcpy(speed_arr[i].filename, FILE_NAME);
+    }
+    gettimeofday(&before_time, NULL);
+    for (int i = 0; i < TEST_COUNTER; i++)
+    {
+        rc = pthread_create(&threads[i], NULL, do_upload, (void *)&speed_arr[i]);
+        if (rc)
+        {
+            printf("cant create thread!\n");
+        }
+    }
+    for (int i = 0; i < TEST_COUNTER; i++)
+    {
+        rc = pthread_join(threads[i], &status);
+        if (rc)
+        {
+            printf("cant join thread!\n");
+        }
+    }
+    gettimeofday(&after_time, NULL);
+    printf("start time: %ld\n", before_time.tv_sec * 1000 + before_time.tv_usec * 1000);
+    printf("start time: %ld\n", after_time.tv_sec * 1000 + after_time.tv_usec * 1000);
+    double time_hand = (double)(calc_past_time(&before_time, &after_time) / (double)1000);
+    for (int i = 0; i < TEST_COUNTER; i++)
+    {
+        sum += (double)(speed_arr[i].speed_download / (double)1024 / (double)1024);
+        time += (double)(speed_arr[i].total_time / (double)1000000);
+    }
+
+    time = time / (double)TEST_COUNTER;
+    printf("Total time: %f seconds\n", time_hand);
+    double final_speed = (double)(10 * (double)TEST_COUNTER / (double)time_hand) * 8;
+    return final_speed;
+}
+
+int main()
+{
+    double download_speed = speedtest_download();
+    printf("Average download speed : %0.2f Mb/s\n", download_speed);
+    double upload_speed = speedtest_upload();
+    printf("Average upload speed : %0.2f Mb/s\n", upload_speed);
+}
